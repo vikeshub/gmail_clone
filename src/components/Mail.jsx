baeb8ae94bcfec7eb86ba49cb6f1acc182ba8b40
vikesh kumar mahto <kumarvikesh287@gmail.com>
2024-10-05 10:22:38 +0530

    navigate

--- src/components/Mail.jsx
+++ src/components/Mail.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { IoMdArrowBack, IoMdMore } from "react-icons/io";
 import {
-  // MdKeyboardArrowLeft,
-  // MdKeyboardArrowRight,
+  MdKeyboardArrowLeft,
+  MdKeyboardArrowRight,
   MdDeleteOutline,
   MdOutlineReport,
   MdOutlineMarkEmailUnread,
@@ -11,12 +11,17 @@ import {
   MdDriveFileMoveOutline,
 } from "react-icons/md";
 import { BiArchiveIn } from "react-icons/bi";
+import { useNavigate } from "react-router-dom";
 function Mail() {
+  const navigate = useNavigate();
   return (
     <div className="flex-1 bg-white rounded-xl mx-5">
       <div className="flex items-center justify-between px-4">
         <div className="flex items-center gap-2 text-gray-700 py-2">
-          <div className="p-2 rounded-full hover:bg-gray-100 cursor-pointer">
+          <div
+            onClick={() => navigate("/")}
+            className="p-2 rounded-full hover:bg-gray-100 cursor-pointer"
+          >
             <IoMdArrowBack size={"20px"} />
           </div>
           <div className="p-2 rounded-full hover:bg-gray-100 cursor-pointer">
@@ -43,6 +48,32 @@ function Mail() {
           <div className="p-2 rounded-full hover:bg-gray-100 cursor-pointer">
             <IoMdMore size={"20px"} />
           </div>
+        </div>
+        <div className="flex items-center gap-2">
+          <button className="hover:rounded-full hover:bg-gray-100">
+            <MdKeyboardArrowLeft size={"24px"} />
+          </button>
+          <button className="hover:rounded-full hover:bg-gray-100">
+            <MdKeyboardArrowRight size={"24px"} />
+          </button>
+        </div>
+      </div>
+      <div className="h=[90vh] overflow-y-auto p-4">
+        <div className="flex items-center justify-between bh-white gap-1">
+          <div className="flex items-center gap-2">
+            <h1 className="text-xl font-medium">Subject</h1>
+            <span className="text-sm bg-gray-200 rounded-md px-2">Inbox</span>
+          </div>
+          <div className="flex-none text-gray-400 my-5 text-sm">
+            <p>12-08-2024</p>
+          </div>
+        </div>
+        <div className="text-gray-500 text-sm">
+          <h1>sherya@gmail.com</h1>
+          <span>to me</span>
+        </div>
+        <div className="my-10">
+          <p>messages</p>
         </div>
       </div>
     </div>
