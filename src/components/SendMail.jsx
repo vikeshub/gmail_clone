af255f8c6b40dd7b3606bcf5ec55878b7f701324
vikesh kumar mahto <kumarvikesh287@gmail.com>
2024-10-05 12:18:03 +0530

    added compoose

--- src/components/SendMail.jsx
+++ src/components/SendMail.jsx
@@ -1,12 +1,24 @@
 import React from "react";
 import { RxCross2 } from "react-icons/rx";
+import { useDispatch, useSelector } from "react-redux";
+import { setOpen } from "../redux/appSlice";
+
 function SendMail() {
-    const open=true;
+    const { open } = useSelector(store => store.app);
+  const dispatch = useDispatch();
+
   return (
-    <div className={`${open?'block':'hidden'}bg-white max-w-6xl shadow-xl shadow-slate-600 rounded-t-md`}>
+    <div
+      className={`${
+        open ? "block" : "hidden"
+      } bg-white max-w-6xl shadow-xl shadow-slate-600 rounded-t-md`}
+    >
       <div className="flex px-3 py-2 bg-[#F2F6Fc] justify-between rounded-t-md">
         <h1>New Message</h1>
-        <div className="p-2 rounded-full hover:bg-gray-200 cursor-pointer">
+        <div
+          onClick={() => dispatch(setOpen(false))}
+          className="p-2 rounded-full hover:bg-gray-200 cursor-pointer"
+        >
           <RxCross2 size={"20px"} />
         </div>
       </div>
@@ -19,11 +31,14 @@ function SendMail() {
         />
         <textarea
           name="message"
-          cols={"30"}
-          rows={"10"}
+          cols="30"
+          rows="10"
           className="outline-none py-1"
         ></textarea>
-        <button type="submit" className="bg-[#0b57d0] rounded-full w-fit px-4 text-white font-medium">
+        <button
+          type="submit"
+          className="bg-[#0b57d0] rounded-full w-fit px-4 text-white font-medium"
+        >
           Send
         </button>
       </form>
